21d11e7d7e0bfd3824f512eb73fb9f26ccdd2f8a
Anaiol <150019444+Anaiol@users.noreply.github.com>
2025-03-27 12:54:02 +0100

    idiomes començats

--- idioma.js
+++ idioma.js
@@ -1,9 +1,13 @@
 IDIOMA=0;
+/* Es una llista de objectes, el index tria el missatge i idiom si es en catala o en castella*/
 MISSATGES = [
     {
-        'Bon dia' : 'Bon dia'
+        'Bon dia' : 'Bon dia',
+        'incorrecte':'incorrecte'
     },
     {
-        'Bon dia' : 'Buenos dias'
+        'Bon dia' : 'Buenos dias',
+        'incorrecte':'incorrecto'
+
     }
 ]
